a3b22b50d44feb8fb00abf42ae1dccbeffbb992f
artin.keyhani@qualyfi.co.uk <artin.keyhani@qualyfi.co.uk>
2023-02-08 12:52:23 +0000

    Modified the Unzipping file

--- Unzipping files.py
+++ Unzipping files.py
@@ -1,22 +1,27 @@
 # Databricks notebook source
 from zipfile import ZipFile
 
-with ZipFile('/dbfs/tmp/Artin/landing/payments.zip','r') as payments:
+!wget "https://github.com/Artin00/Bikestartschema123/raw/main/payments.zip" -P "/dbfs/tmp/Artin/github"
+!wget "https://github.com/Artin00/Bikestartschema123/raw/main/trips.zip" -P "/dbfs/tmp/Artin/github"
+!wget "https://github.com/Artin00/Bikestartschema123/raw/main/stations.zip" -P "/dbfs/tmp/Artin/github"
+!wget "https://github.com/Artin00/Bikestartschema123/raw/main/riders.zip" -P "/dbfs/tmp/Artin/github"
+
+with ZipFile('/dbfs/tmp/Artin/github/payments.zip','r') as payments:
     
     payments.extract("payments.csv", path='/dbfs/tmp/Artin/landing_2')
 payments.close()
 
-with ZipFile('/dbfs/tmp/Artin/landing/riders.zip','r') as riders:
+with ZipFile('/dbfs/tmp/Artin/github/riders.zip','r') as riders:
     
     riders.extract("riders.csv", path='/dbfs/tmp/Artin/landing_2')
 riders.close()
 
-with ZipFile('/dbfs/tmp/Artin/landing/trips.zip','r') as trips:
+with ZipFile('/dbfs/tmp/Artin/github/trips.zip','r') as trips:
     
     trips.extract("trips.csv", path='/dbfs/tmp/Artin/landing_2')
 trips.close()
 
-with ZipFile('/dbfs/tmp/Artin/landing/stations.zip','r') as stations:
+with ZipFile('/dbfs/tmp/Artin/github/stations.zip','r') as stations:
     
     stations.extract("stations.csv", path='/dbfs/tmp/Artin/landing_2')
 stations.close()
